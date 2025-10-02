e7d6feda869cb517ba93a40a8238d4d9822b7097
levieohlund <levie.ohlund@chasacademy.se>
2025-10-02 14:58:41 +0200

    Menu for monitoring system
    First glance of a menu for the upcoming project made an main menu.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Huvudmeny för för övervakningssystemet och larm
 
 import time
-import monitor  # Add this import
+import monitor  
 
 #Första menyn som även är huvumenyn i programmet 
 def main_menu():
@@ -20,7 +20,7 @@ def main_menu():
             print("Exiting...")
             break
         elif choice == "1":
-            # Fix these lines - they have syntax errors
+            
             cpu = monitor.get_cpu_usage()
             mem_pct, mem_used, mem_total = monitor.get_memory_usage()
             disk_pct, disk_used, disk_total = monitor.get_disk_usage()
